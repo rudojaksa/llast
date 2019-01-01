078e3e4aeaf1f6590fb2d47aa5fc8f281d69f43b
Rudolf JAKSA <rudolf.jaksa@gmail.com>
2019-01-02 00:21:18 +0100

    update

--- llast.pl
+++ llast.pl
@@ -5,7 +5,6 @@ use Time::HiRes qw(usleep time stat);
 # include "datetime.pl"
 # ---------------------------------------------------------------------------------------------- HELP
 
-
 $HELP=<<EOF;
 
 NAME
